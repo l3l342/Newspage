bdfb7c352b4c3610dda72b391337c238725eb42d
Brottyp <ben.dr@gmx.net>
2021-05-13 15:16:11 +0200

    just one time load wheel, cause of session storages

--- scripts/loader.js
+++ scripts/loader.js
@@ -2,9 +2,17 @@ var myVar;
 
 function loader() {
   myVar = setTimeout(showPage, 2500);
+  sessionStorage.setItem("loader_bool", 1);
 }
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("all").style.display = "block";
+}
+
+const currentbool = sessionStorage.getItem("loader_bool");
+if ( currentbool == 1 ) {
+  showPage();
+  element = document.getElementsByClassName("all")
+  element[0].style.webkitAnimationName = "none";
 }
